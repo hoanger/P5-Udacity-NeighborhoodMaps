49f51ac7515ce80dca53834d568a58b952ef4d99
Andy Nguyen <nguyenandyh@gmail.com>
2016-01-03 01:19:48 -0800

    refactor: Create private function for bouncing markers

--- js/maps.js
+++ js/maps.js
@@ -6,9 +6,7 @@
 			this.googleMap = new google.maps.Map(el, opts);
 			this.markers = new Markerer();
 			// Google best practices call for only one info window to reduce clutter
-			this.infowindow = new google.maps.InfoWindow({
-    			content: ''
-  			});
+			this.infowindow = new google.maps.InfoWindow();
 		}
 		Mapper.prototype = {
 			// create a marker and add to the markers array
@@ -20,12 +18,9 @@
 				this.markers.add(marker);
 				// add listener to marker to bounce and display info window
 				marker.addListener('click', function(){
-					marker.setAnimation(google.maps.Animation.BOUNCE);
-					window.setTimeout(function(){
-						marker.setAnimation(null);
-					}, 700);
-					self.infowindow.setContent(marker.id);
-					self.infowindow.open(self.googleMap, marker);
+					self._bounceMarker(marker, 700);
+					self.infowindow.setContent(this.id);
+					self.infowindow.open(self.googleMap, this);
 				});
 			},
 			// set marker visibility to newVal
@@ -38,14 +33,19 @@
 			},
 			// bounce a marker for a short moment
 			bounceMarker: function(callback) {
+				var self = this;
 				this.markers.find(callback, function(markers) {
 					markers.forEach(function(marker){
-						marker.setAnimation(google.maps.Animation.BOUNCE);
-						window.setTimeout(function(){
-							marker.setAnimation(null);
-						}, 1400);
+						self._bounceMarker(marker, 1400);
 					});
 				});
+			},
+			// private bounce marker function
+			_bounceMarker: function(mkr, time) {
+				mkr.setAnimation(google.maps.Animation.BOUNCE);
+					window.setTimeout(function(){
+					mkr.setAnimation(null);
+				}, time);
 			}
 		};
 		return Mapper;
